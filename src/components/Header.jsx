f022c0ecd7183ffe33010618c2c9aa05a0d79255
MeyTeplon <ASUS@LAPTOP-VTDSD35M>
2024-05-13 15:26:41 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-bootstrap";
+
+
+function Header() {
+    return(
+        <div>ini header
+            <NavLink to="home" 
+        >Home</NavLink>
+        </div>
+    )
+}
+
+export default Header;
